7a9422652efe298b58153c4f80b38dda0d0c9b45
kyle-peterson-1 <pet23003@byui.edu>
2023-11-10 18:42:09 -0700

    james bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Promt the user for their first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Promt the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //Display the name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
