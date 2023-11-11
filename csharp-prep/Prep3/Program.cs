3f5bb0f197c6fab9826f8712b8e66580fb9343ca
kyle-peterson-1 <pet23003@byui.edu>
2023-11-10 20:00:32 -0700

    Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Create a random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("I am thinking of a number between 1 and 100. I want you to guess the number I am thinking.");
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("thats it!");
+                Console.WriteLine("Good job!");
+            }
+        }
     }
+
 }
