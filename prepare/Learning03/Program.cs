fdb3f46c943b51b43341903331cb181a6da2edc9
kyle-peterson-1 <pet23003@byui.edu>
2023-11-26 19:06:55 -0700

    added prove W03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,93 @@
 using System;
 
+public class Fraction
+{
+    private int top;
+    private int bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        this.top = top;
+        bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return top;
+    }
+
+    public void SetTop(int top)
+    {
+        this.top = top;
+    }
+
+    public int GetBottom()
+    {
+        return bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            this.bottom = bottom;
+        }
+        else
+        {
+            Console.WriteLine("Denominator cannot be zero.");
+        }
+    }
+
+    // Methods to return representations
+    public string GetFractionString()
+    {
+        return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / bottom;
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Creating instances using different constructors
+        Fraction fraction1 = new Fraction();         // 1/1
+        Fraction fraction2 = new Fraction(6);        // 6/1
+        Fraction fraction3 = new Fraction(6, 7);     // 6/7
+
+        // Displaying fractions
+        Console.WriteLine(fraction1.GetFractionString());  // 1/1
+        Console.WriteLine(fraction2.GetFractionString());  // 6/1
+        Console.WriteLine(fraction3.GetFractionString());  // 6/7
+
+        // Changing values using setters
+        fraction1.SetTop(3);
+        fraction1.SetBottom(4);
+
+        // Displaying the modified fraction
+        Console.WriteLine(fraction1.GetFractionString());  // 3/4
+
+        // Displaying decimal values
+        Console.WriteLine(fraction1.GetDecimalValue());    // 0.75
+        Console.WriteLine(fraction2.GetDecimalValue());    // 6.0
+        Console.WriteLine(fraction3.GetDecimalValue());    // 0.8571428571428571
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,120 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Sample scripture
+        Scripture scripture = new Scripture("John 3:16", "For God so loved the world, that he gave his only Son, " +
+            "that whoever believes in him should not perish but have eternal life.");
+
+        do
+        {
+            Console.Clear();
+            scripture.Display(); // Display the scripture
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to end.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(2); // Hide 2 random words
+
+        } while (!scripture.AllWordsHidden());
+    }
+}
+
+// class for the scriptures 
+class Scripture
+{
+    private Reference reference;  
+    private List<Word> words;             
+    private List<int> hiddenWordIndices;  
+
+    // initialize the scripture
+    public Scripture(string referenceText, string scriptureText)
+    {
+        reference = new Reference(referenceText);
+        words = scriptureText.Split(' ').Select(word => new Word(word)).ToList();
+        hiddenWordIndices = new List<int>();
+    }
+
+    // hide words from the scripture.
+    public void Display()
+    {
+        Console.WriteLine($"{reference.GetDisplayText()}\n");
+
+        for (int i = 0; i < words.Count; i++)
+        {
+            if (hiddenWordIndices.Contains(i))
+                Console.Write("____ "); // Display an underscore for hidden words
+            else
+                Console.Write($"{words[i].GetDisplayText()} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    // Hide a specified number of random words in the scripture
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++)
+        {
+            int randomIndex;
+            do
+            {
+                randomIndex = random.Next(words.Count);
+            } while (hiddenWordIndices.Contains(randomIndex));
+
+            hiddenWordIndices.Add(randomIndex);
+        }
+    }
+
+    // Check if all words in the scripture are hidden
+    public bool AllWordsHidden()
+    {
+        return hiddenWordIndices.Count == words.Count;
+    }
+}
+
+// Class representing the reference of the scripture
+class Reference
+{
+    private string book;       
+    private int chapter;        
+    private int startVerse;    
+    private int endVerse;      
+
+    public Reference(string book)
+    {
+        this.book = book;
+        this.chapter = 1;
+        this.startVerse = 1;
+        this.endVerse = 1;
+    }
+
+    // Get the display text for the reference
+    public string GetDisplayText()
+    {
+        return $"{book} {chapter}:{startVerse}-{endVerse}";
+    }
+}
+
+class Word
+{
+    private string text;
+
+    public Word(string text)
+    {
+        this.text = text;
+    }
+
+    public string GetDisplayText()
+    {
+        return text;
     }
 }
